6ed9223189b0050d0290b82145c5e3a737cec922
2603003199a <58811224+skybird23333@users.noreply.github.com>
2022-06-22 12:33:57 +0800

    Fix: Unused Button

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Button } from "../components/Button"
 
 export class IndexRoute extends React.Component {
     render() {
