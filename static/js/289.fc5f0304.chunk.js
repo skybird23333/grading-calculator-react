c9f1720960f9254f9f4ccd6059c30925fb14b016
skybird23333 <skybird23333@users.noreply.github.com>
2024-07-22 11:10:11 +0000

    deploy: 6fc6fe219e27dda2797a41863f93e2c745a0eb05

---
+++ static/js/289.fc5f0304.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[289],{289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.fc5f0304.chunk.js.map
